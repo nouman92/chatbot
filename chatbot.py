d82be03121fc3c417c711a151f49e4ac15c5d456
nouman <nouman92@live.com>
2017-05-29 00:52:13 +0500

    - environ fix

--- chatbot.py
+++ chatbot.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route("/", methods=['GET'])
 def verify():
     if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
-        if not request.args.get("hub.verify_token") == os.getenviron["VERIFY_TOKEN"]:
+        if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
             return "Verification token mismatch",403
         return request.args["hub.challange"],200
     return "Hello Bot", 200
